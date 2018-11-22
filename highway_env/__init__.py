ac0c2b18d453c4bb5a4a90f9e9664dd409ff61a8
JadeQG <35579892+JadeQG@users.noreply.github.com>
2018-11-22 15:03:10 -0800

    Update __init__.py

--- highway_env/__init__.py
+++ highway_env/__init__.py
@@ -4,13 +4,3 @@ register(
     id='highway-v0',
     entry_point='highway_env.envs:HighwayEnv',
 )
-
-register(
-    id='highway-merge-v0',
-    entry_point='highway_env.envs:MergeEnv',
-)
-
-register(
-    id='highway-roundabout-v0',
-    entry_point='highway_env.envs:RoundaboutEnv',
-)
